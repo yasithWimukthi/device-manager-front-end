1f77ca01d06aa37d02672ef1a82719389d7347fb
yasithw <yasith.w@eight25media.com>
2023-11-23 19:28:20 +0530

    implement location page

--- src/pages/LocationPage/index.js
+++ src/pages/LocationPage/index.js
@@ -3,10 +3,12 @@ import PageHeader from "../../components/PageHeader/PageHeader";
 import LocationForm from "./LocationForm";
 import {Button} from "@mui/material";
 import axios from '../../services/axiosConfig';
+import LocationTable from "./LocationTable";
 
 const LocationPage = () => {
     const [isLocationFormOpen, setIsLocationFormOpen] = useState(false);
     const [devices, setDevices] = useState([]);
+    // const [locations, setLocations] = useState([]);
 
     const handleLocationFormOpen = () => {
         setIsLocationFormOpen(true);
@@ -46,7 +48,40 @@ const LocationPage = () => {
             .catch(error => {
                 console.log(error);
             });
+
+        // fetch all locations here
+        axios.get('/locations')
+            .then(response => {
+                console.log(response);
+                setLocations(response.data.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }, []);
+
+    const [locations, setLocations] = useState([
+        {
+            id: 1,
+            serial_number: 'ABC123',
+            name: 'Location A',
+            ip_address: '192.168.1.1',
+            devices: [
+                { id: 1, name: 'Device 1' },
+                { id: 2, name: 'Device 2' },
+            ],
+        },
+        // Add more locations as needed
+    ]);
+
+    const handleAddDevice = (locationId) => {
+        // Implement your logic to add a device for the specified location
+        // Update the state accordingly
+    };
+
+    const handleRemoveDevice = (locationId) => {
+        console.log('Remove device for location:', locationId);
+    };
 
     return (
         <div>
@@ -54,13 +89,19 @@ const LocationPage = () => {
             <Button variant="contained" color="primary" onClick={handleLocationFormOpen}>
                 Open Location Form
             </Button>
-
             <LocationForm
                 open={isLocationFormOpen}
                 devices={devices}
                 onClose={handleLocationFormClose}
                 onSubmit={handleLocationFormSubmit}
             />
+            <div>
+                <LocationTable
+                    locations={locations}
+                    onAddDevice={handleAddDevice}
+                    onRemoveDevice={handleRemoveDevice}
+                />
+            </div>
         </div>
     )
 }
